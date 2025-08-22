c2b95aed6cccbe02e2a791d13b65e62b5510c7c1
nickvorobev <nick_vorobev@mail.com>
2025-08-22 19:42:00 +0300

    Описание изменений

---
+++ app.py
@@ -0,0 +1,16 @@
+import gradio as gr
+#заглушка. Возвращает запрос.
+def fake_rag(query):
+	return f" you ask:'{query}'. Хороший вопрос, но RAG сервер еще не готов."
+# Веб-интерфейс
+demo = gr.Interface(
+	fn=fake_rag,
+	inputs=gr.Textbox(lines=2, placeholder="Введите ваш вопрос здесь..."),
+	outputs="text",
+	title="RAG Ассистент",
+	description="Тест веб-сервера без RAG."
+)
+
+# Запуск сервера
+if __name__ =="__main__":
+	demo.launch(server_name="0.0.0.0", server_port=7860, share=True)
