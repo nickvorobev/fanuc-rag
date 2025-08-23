b52c9197884fce4a16b478428f17387397dfb1f0
nickvorobev <nick_vorobev@mail.com>
2025-08-23 18:21:21 +0300

    тест изменений 3

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import gradio as gr
 #заглушка. Возвращает запрос.
 def fake_rag(query):
-	return f" you ask:'{query}'. Хороший вопрос, но RAG сервер non0."
+	return f" you ask:'{query}'. Хороший вопрос, но RAG сервер еще не создан."
 # Веб-интерфейс
 demo = gr.Interface(
 	fn=fake_rag,
